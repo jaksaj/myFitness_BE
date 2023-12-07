40a7247784ee0eda63fd40f42075c6cf7576d372
jaksaj <jaksaj35@gmail.com>
2023-12-07 17:57:11 +0100

    Linked to DB and created user model

--- index.js
+++ index.js
@@ -11,3 +11,31 @@ app.get('/', (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
+
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://127.0.0.1:27017/myFitness');
+
+const db = mongoose.connection;
+
+db.on('error', (error) => {
+    console.error('Greška pri spajanju:', error);
+});
+db.once('open', function() {
+  console.log('Spojeni smo na MongoDB bazu');
+});
+
+const { Schema } = mongoose;
+
+const userSchema = new Schema({
+  username: { type: String, required: true },
+  email: { type: String, required: true },
+  password: { type: String, required: true },
+});
+
+const User = mongoose.model("User", userSchema);
+const newUser = new User({
+  username: "mate",
+  email: 'mate@pmfst.hr',
+  password: "ohvaho490vs",
+});
+newUser.save();
