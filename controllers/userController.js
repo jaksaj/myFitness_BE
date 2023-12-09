fc232dc3de0521166ea8e43c2155790900476d60
MateElez <elez.mate1@gmail.com>
2023-12-09 15:13:14 +0100

    Dodan log in i ruta na njega

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,6 @@
 const User = require("../models/user");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 const createUser = async (req, res) => {
   try {
@@ -13,12 +15,47 @@ const createUser = async (req, res) => {
     const savedUser = await newUser.save();
 
     res.status(201).json(savedUser);
+
+    const token = jwt.sign({userId: user._id}, "your-secret-key", {
+      expiresIn: "1h"
+    })
+    
+    res.status(200).json({ message: "Login successful", user, token });
+
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: "Internal Server Error" });
   }
 };
 
+const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email })
+
+    if (!User) {
+      return res.status(401).json({ error: "Invalid credentials" });
+    }
+
+    const passwordMatch = await bcrypt.compare(password, user.password);
+
+    if (!passwordMatch) {
+      return res.status(401).json({ error: "Invalid credentials" });
+    }
+
+    const token = jwt.sign({userId: user._id}, "your-secret-key", {
+      expiresIn: "1h"
+    })
+    res.status(200).json({ message: "Login successful", user, token });
+  }
+  catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+}
+
 module.exports = {
   createUser,
+  loginUser
 };
--- routes/user.js
+++ routes/user.js
@@ -3,5 +3,5 @@ const router = express.Router();
 const UserController = require("../controllers/userController");
 
 router.post("/users", UserController.createUser);
-
+router.post("/users", UserController.loginUser);
 module.exports = router;
