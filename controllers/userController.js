2dda5f6f501976e62887723e38726ba7fe64898d
jaksaj <jaksaj35@gmail.com>
2023-12-09 19:20:50 +0100

    fixed errors

--- controllers/userController.js
+++ controllers/userController.js
@@ -14,14 +14,11 @@ const createUser = async (req, res) => {
 
     const savedUser = await newUser.save();
 
-    res.status(201).json(savedUser);
+    const token = jwt.sign({ userId: savedUser._id }, "your-secret-key", {
+      expiresIn: "1h",
+    });
 
-    const token = jwt.sign({userId: user._id}, "your-secret-key", {
-      expiresIn: "1h"
-    })
-    
-    res.status(200).json({ message: "Login successful", user, token });
-
+    res.status(200).json({ message: "Login successful", savedUser, token });
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: "Internal Server Error" });
@@ -30,32 +27,29 @@ const createUser = async (req, res) => {
 
 const loginUser = async (req, res) => {
   try {
-    const { email, password } = req.body;
-
-    const user = await User.findOne({ email })
-
-    if (!User) {
+    const { email, password } = req.body.userData;
+    const user = await User.findOne({ email: email });
+    if (!user) {
       return res.status(401).json({ error: "Invalid credentials" });
     }
 
-    const passwordMatch = await bcrypt.compare(password, user.password);
-
-    if (!passwordMatch) {
+    //const passwordMatch = await bcrypt.compare(password, user.password);
+    // TODO implement bcrypt
+    if (password !== user.password) {
       return res.status(401).json({ error: "Invalid credentials" });
     }
 
-    const token = jwt.sign({userId: user._id}, "your-secret-key", {
-      expiresIn: "1h"
-    })
+    const token = jwt.sign({ userId: user._id }, "your-secret-key", {
+      expiresIn: "1h",
+    });
     res.status(200).json({ message: "Login successful", user, token });
-  }
-  catch (error) {
+  } catch (error) {
     console.error(error);
     res.status(500).json({ error: "Internal Server Error" });
   }
-}
+};
 
 module.exports = {
   createUser,
-  loginUser
+  loginUser,
 };
